ea6bf5c91ab554029c93ebc7078b225637c86365
Ari Halonen <ari.halonen@tieto.com>
2024-08-13 13:07:40 +0300

    osa11: fixed linting errors

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+require("dotenv").config();
 const express = require("express");
 const app = express();
 
@@ -7,5 +8,6 @@ const PORT = process.env.PORT || 5000;
 app.use(express.static("dist"));
 
 app.listen(PORT, () => {
+  // eslint-disable-next-line no-console
   console.log(`server started on port ${PORT}`);
 });
